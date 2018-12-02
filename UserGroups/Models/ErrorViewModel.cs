31a1167c8d42ab0fa6aafe268eefac271a315d26
Adam Lancaster <arglanc@users.noreply.github.com>
2018-12-02 15:47:48 +0000

    Initialise VS solution

---
+++ UserGroups/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UserGroups.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
