b64bb3c0975736ae47a52c547cadafac1a6dedc1
Adam Lancaster <arglanc@users.noreply.github.com>
2018-12-07 12:11:02 +0000

    Create gatekeeper API client

---
+++ UserGroups/Services/IGatekeeperApiClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UserGroups.Services
+{
+    public interface IGatekeeperApiClient
+    {
+        Task<HttpResponseMessage> GetAsync(string path);
+
+        Task<HttpResponseMessage> PostAsync(string path, object body);
+    }
+}
